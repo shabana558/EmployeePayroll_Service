d907f010a21800c0ac7b189c71caef4b73496abb
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-24 16:35:53 +0530

    [shabana]CreateDatabase

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -1,6 +1,35 @@
-/********script for selectTopNRows command from SSMS *******/
-SELECT TOP (1000) [Id]
-          ,[Name]
-		  ,[Salary]
-		  ,[startDate]
-FROM [Payroll_Service].[dbo].[Employee_Payroll]
+--------------------UC1 create database------------------
+Create database Payroll_Service;
+use Payroll_Service;
+---------------------UC2 Create table----------------------
+CREATE TABLE Employee_Payroll (
+      Id int identity(1,1) primary key,
+	  Name varchar(200),
+	  Salary float,
+	  StartDate date
+	  );
+-------create-add (c)
+INSERT INTO Employee_Payroll (Name,Salary,StartDate) VALUES('Sachin',64634.56, '2020-01-01')
+INSERT INTO Employee_Payroll (Name,StartDate) VALUES('Sachin', getdate());
+INSERT INTO Employee_Payroll (Name,StartDate) VALUES('Dhoni', getdate());
+INSERT INTO Employee_Payroll (Name,Salary,StartDate) VALUES('Dhoni',350000.36,'2020-05-05')
+
+
+
+
+
+-------create-read-(R)
+select * from Employee_Payroll Where Name='Dhoni';
+select Id,Name from Employee_Payroll where Name='Dhoni';
+select Id,Name from Employee_Payroll where Name='Dhoni'
+
+select salary from Employee_Payroll where Name='Dhoni';
+select * from Employee_Payroll;
+select Salary,Name from Employee_Payroll where startDate between CAST('2020-01-01' as Date) and getdate();
+
+
+
+	  
+
+
+	  
