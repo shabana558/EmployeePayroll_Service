cfb2ad2b8455211c6fd04e0a1efe5f4da6be5b45
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-24 16:50:00 +0530

    [shabana]UC2-andUC3-InsertintoEmplyeepayroll

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -9,21 +9,22 @@ CREATE TABLE Employee_Payroll (
 	  StartDate date
 	  );
 -------create-add (c)
+------------UC3-Insert into Employee_payroll---------
 INSERT INTO Employee_Payroll (Name,Salary,StartDate) VALUES('Sachin',64634.56, '2020-01-01')
 INSERT INTO Employee_Payroll (Name,StartDate) VALUES('Sachin', getdate());
 INSERT INTO Employee_Payroll (Name,StartDate) VALUES('Dhoni', getdate());
 INSERT INTO Employee_Payroll (Name,Salary,StartDate) VALUES('Dhoni',350000.36,'2020-05-05')
-
-
-
-
+INSERT INTO Employee_Payroll (Name,Salary,StartDate)VALUES('shabana',62000.63,'2018-01-01')
 
 -------create-read-(R)
 select * from Employee_Payroll Where Name='Dhoni';
 select Id,Name from Employee_Payroll where Name='Dhoni';
 select Id,Name from Employee_Payroll where Name='Dhoni'
 
+---------UC4-Select from Employee_Payroll----
 select salary from Employee_Payroll where Name='Dhoni';
+select salary from Employee_Payroll where Name='shabana';
+
 select * from Employee_Payroll;
 select Salary,Name from Employee_Payroll where startDate between CAST('2020-01-01' as Date) and getdate();
 
